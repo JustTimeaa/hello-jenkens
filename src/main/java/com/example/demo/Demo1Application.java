e4232e6a33e15c1dc33f1e963787c13fc03a3d5f
pmm <373844670@qq.com>
2021-07-18 14:41:43 +0800

    第三次333333333

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,12 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 @SpringBootApplication
 public class Demo1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(Demo1Application.class, args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        System.out.println("\n----------------------------------------------------------\n\t" +
+                "项目启动启动成功! Access URLs:\n\t" +
+                "Local访问网址: \thttp://localhost:" + port + path + "\n\t" +
+                "  接口文档地址: \thttp://" + ip + ":" + port + path +"/swagger-ui.html"+ "\n\t" +
+                "----------------------------------------------------------");
     }
 
 }
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags ="测试金肯四" )
+public class TestController {
+
+    @GetMapping(value = "/test")
+    @ApiOperation("cesui")
+    public String test(){
+        return "测试 Jenkis";
+    }
+}
